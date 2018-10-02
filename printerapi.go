d277936af73bffad9fe731ae7e97b6cd652c5a26
Simon Tucker <brotherlogic@gmail.com>
2018-10-02 16:38:12 -0700

    Fixes bug in printer api

--- printerapi.go
+++ printerapi.go
@@ -6,5 +6,5 @@ import pb "github.com/brotherlogic/printer/proto"
 // Print performs a print
 func (s *Server) Print(ctx context.Context, req *pb.PrintRequest) (*pb.PrintResponse, error) {
 	err := s.localPrint(req.Text)
-	return nil, err
+	return &pb.PrintResponse{}, err
 }
