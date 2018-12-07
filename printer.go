d796d21e8c3c40fc35cd75fbf25b10c746d8dcec
Simon Tucker <simon@brotherlogic@gmail>
2018-12-07 09:58:47 -0800

    Turn off printer

--- printer.go
+++ printer.go
@@ -68,7 +68,7 @@ func (s *Server) localPrint(text string, lines []string, ti time.Time) error {
 func Init() *Server {
 	s := &Server{
 		&goserver.GoServer{},
-		true, //Print enabled
+		false, //Print enabled
 		0,
 	}
 	return s
