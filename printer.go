64bc82f12c0204e30d9f2a78a9b094d5f8fe7713
Simon Tucker <brotherlogic@gmail.com>
2020-07-03 15:39:33 -0700

    Better logging

--- printer.go
+++ printer.go
@@ -100,7 +100,7 @@ func (s *Server) localPrint(text string, lines []string, ti time.Time) (time.Dur
 	cmd.Start()
 	err = cmd.Wait()
 
-	s.Log(fmt.Sprintf("OUTPUT = %v", output))
+	s.Log(fmt.Sprintf("OUTPUT = %v from %v -> %v", output, text, lines))
 	return time.Minute, err
 }
 
