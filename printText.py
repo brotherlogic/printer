a481467e29185a2303d9b99e0e9f3a93e28e3f7e
Simon Tucker <brotherlogic@gmail.com>
2021-05-06 18:16:21 -0700

    Ensures we don't send a print when there's no paper

--- printText.py
+++ printText.py
@@ -5,6 +5,9 @@ import sys
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
+if not printer.hasPaper():
+    sys.exit(1)
+    
 for line in sys.argv[1:]:
     printer.println(line)
 printer.feed(4)
