0c4efc143518e7bbe0f25045b45c9f8b656d9401
simon <brotherlogic@gmail.com>
2021-06-26 17:34:46 -0700

    Adjust label

--- printer.go
+++ printer.go
@@ -101,7 +101,7 @@ func (s *Server) localPrint(text string, lines []string, ti time.Time) (time.Dur
 	out, err := cmd.StdoutPipe()
 
 	if err != nil {
-		s.Log(fmt.Sprintf("Error from stdout: %v", err))
+		s.Log(fmt.Sprintf("Error in stdout: %v", err))
 	}
 
 	if out != nil {
