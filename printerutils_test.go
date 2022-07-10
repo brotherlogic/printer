2e7e2954f94e06712aa97a0348b99fca5df10d9f
Simon Tucker <brotherlogic@gmail.com>
2022-07-10 08:47:57 -0700

    Achieving full coverage

--- printerutils_test.go
+++ printerutils_test.go
@@ -15,3 +15,13 @@ func TestBadDequeue(t *testing.T) {
 		t.Errorf("Bad dequeue did not fail")
 	}
 }
+
+func TestBadProcessPrint(t *testing.T) {
+	s := InitTestServer()
+	s.GoServer.KSclient.Fail = true
+
+	_, err := s.processPrint(context.Background(), nil)
+	if err != nil {
+		t.Errorf("Bad process did not fail")
+	}
+}
