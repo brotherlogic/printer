24b312f1c3f5f6ae3d204fb3fe24884c32e4235b
Simon Tucker <brotherlogic@gmail.com>
2023-07-25 08:52:18 -0700

    Support ping

--- printerapi.go
+++ printerapi.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
+	"strings"
 	"time"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/brotherlogic/printer/proto"
 )
+
+func (s *Server) Ping(ctx context.Context, req *pb.PingRequest) (*pb.PingResponse, error) {
+	data, err := exec.Command("/usr/bin/lsusb").Output()
+	if err != nil {
+		return nil, fmt.Errorf("error listing usb components: %v", err)
+	}
+	s.CtxLog(ctx, fmt.Sprintf("USBRES: %v", string(data)))
+	if strings.Contains(string(data), "TSP100II") {
+		return &pb.PingResponse{}, nil
+	}
+
+	return nil, status.Errorf(codes.FailedPrecondition, "printer is not available")
+}
 
 // Print performs a print
 func (s *Server) Print(ctx context.Context, req *pb.PrintRequest) (*pb.PrintResponse, error) {
@@ -52,7 +70,7 @@ func (s *Server) Clear(ctx context.Context, req *pb.ClearRequest) (*pb.ClearResp
 	return &pb.ClearResponse{}, s.save(ctx, config)
 }
 
-//List lists the backlog
+// List lists the backlog
 func (s *Server) List(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
 	config, err := s.load(ctx)
 	if err != nil {
